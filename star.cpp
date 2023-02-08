f0dc262023793f127d4b13c6290f0b97d3dc329f
MrZodoxVpython <mrzodoxvpython@gmail.com>
2023-02-08 10:32:45 +0000

    deleted trash

--- star.cpp
+++ star.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
-
-int main() {
-for (int i = 0; i < 5; ++i) {
-for (int j = 0; j < 10; ++j) {
-if (i == 0 || i == 4 || j == 0 || j == 9 - i) {
-std::cout << "*";
-} else {
-std::cout << " ";
-}
-}
-std::cout << std::endl;
-}
-return 0;
+using namespace std;
+int main(){
+	cout<<"hello world";
+	return 0;
 }
