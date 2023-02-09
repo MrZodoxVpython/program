82097e8b2b7e0f400fa0a4a4ce4c97d1202d7b33
MrZodoxVpython <mrzodoxvpython@gmail.com>
2023-02-09 06:39:06 +0000

    first commit

--- sudo.cpp
+++ sudo.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout<< "Halo,Dunia" << endl;
+	cout << "Hello, I start learning cpp again to getting more remember" << endl;
+	for(int i = 0; i < 10; i++) {
+		cout << "learning cpp" << endl;
+	}
+	return 0;
 }
