3a74a6dfa1ec0dac59be7f95feb5680a32c2ad61
MrZodoxVpython <mrzodoxvpython@gmail.com>
2023-02-09 06:54:01 +0000

    added comment

--- sudo.cpp
+++ sudo.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-using namespace std;
-int main(){
+#include <iostream> //this is a part deklarasi of include or actually the mean is for library
+using namespace std; //this one optional chose we use std because function in library iostream covered by std,if not use std you should std before cout or cin
+int main(){ //this part is an deklarasi of function 
 	cout << "Hello, I start learning cpp again to getting more remember" << endl;
 	for(int i = 0; i < 10; i++) {
 		cout << "learning cpp" << endl;
+		
 	}
+	
 	return 0;
 }
